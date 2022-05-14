05a0dab0f9f8597e16eb76e4c83541cedf6f17a2
leoNavarro95 <potty3800@gmail.com>
2022-05-14 17:14:51 -0500

    comment and post components design

--- src/modules/flip-game/components/Comment.vue
+++ src/modules/flip-game/components/Comment.vue
@@ -3,8 +3,13 @@
 </script>
 
 <template>
-  <div>
-    <div v-for="comment in comments" :key="comment.id">
+  <div class="md:m-5 mt-5 space-y-5 grid grid-cols-1 content-center font-sans text-xl">
+    <div v-for="comment in comments" :key="comment.id"
+        class="p-6 max-w-max rounded-t-full border border-gray-200 shadow-md" 
+        :class="(comment.id % 2 == 0) ? 
+                ['bg-violet-300', 'rounded-br-full', 'text-left'] : 
+                ['bg-violet-200', 'rounded-bl-full', 'text-right']"
+    >
       <h3>{{ comment.name }}</h3>
       <p>{{ comment.body }}</p>
     </div>
--- src/modules/flip-game/components/Post.vue
+++ src/modules/flip-game/components/Post.vue
@@ -9,20 +9,31 @@
   const { getPostComments } = storeToRefs(useCommentStore())
   const { fetchComments } = useCommentStore()
 
+  const firstLetterUpperCase = (str: string) => `${str?.charAt(0).toUpperCase()}${str?.substring(1)}` 
+
   fetchComments()
 </script>
 
 <template>
-  <div>
-    <div>
-      <h2>{{ post.title }}</h2>
-      <p v-if="author">Written by: <RouterLink :to="`/author/${author.username}`">{{ author.name }}</RouterLink>
-        | <span>Comments: {{ getPostComments.length }}</span>
-      </p>
-      <p>{{ post.body }}</p>
+    <div class="m-5 pt-6 px-6 pb-0 bg-violet-300 rounded-lg border border-gray-200 shadow-md">
+        <h2 class="font-sans font-normal text-2xl text-left">
+            "{{ firstLetterUpperCase(post.title) }}"
+        </h2>
+        
+        <div class="my-2 w-full border-t-2 border-gray-300"></div>
+        <p class="font-sans font-medium text-sm text-justify">{{ firstLetterUpperCase(post.body) }}</p>
+        
+        <p class="text-right" v-if="author">Written by: 
+        <RouterLink class="hover:text-blue-800 font-bold underline" :to="`/author/${author.username}`">
+            {{ author.name }}
+        </RouterLink>
+            <span> | Comments: {{ getPostComments.length }}</span>
+        </p>
     </div>
-    <hr>
-    <h3>Comments:</h3>
-    <comment :comments="getPostComments"></comment>
-  </div>
+    
+    <div class="p-5">
+        <div class="mt-2 mb-0 w-full border-t-2 border-violet-400"></div>
+        <h3 class="font-sans font-normal text-2xl text-center bg-violet-100">Comments:</h3>
+        <comment :comments="getPostComments"></comment>
+    </div>
 </template>
