0a964a6f8f6176b076abb05b7beab82ece255b30
leoNavarro95 <potty3800@gmail.com>
2022-05-14 12:11:46 -0500

    author store created

---
+++ src/modules/flip-game/stores/author.ts
@@ -0,0 +1,28 @@
+import { defineStore } from 'pinia'
+import { usePostStore } from './post'
+
+interface User{
+    id: number,
+    name: string,
+    username: string,
+    email: string
+}
+
+export const useAuthorStore = defineStore({
+  id: 'author',
+  state: () => ({
+    authors: [{} as User]
+  }),
+  getters: {
+    getPostAuthor: (state) => {
+      const postStore = usePostStore()
+      return state.authors.find((author: User) => author.id === postStore.post.userId)
+    }
+  },
+  actions: {
+    async fetchAuthors() {
+      this.authors = await fetch('https://jsonplaceholder.typicode.com/users')
+                            .then((response) => response.json())
+    }
+  }
+})
