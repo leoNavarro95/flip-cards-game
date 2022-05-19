02c30129b8f1034fb9bc0726672ef7a6137b7247
leoNavarro95 <potty3800@gmail.com>
2022-05-19 10:41:04 -0500

    added single author views

---
+++ src/modules/flip-game/components/Author.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+import { RouterLink } from 'vue-router'
+
+defineProps(['author', 'posts'])
+
+</script>
+
+
+<template>
+    <h1> Author component</h1>
+    <h1> {{ author.name }}</h1>
+    
+    <p> {{ posts.length }} posts written. </p>
+    <div v-for="post in posts" :key="post.id" >
+        <div>{{ post.title }}</div>
+        <router-link :to="`/post/${post.id}`"> detalles</router-link>
+    </div>
+</template>
---
+++ src/modules/flip-game/views/SingleAuthorView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+  import { computed } from 'vue'
+  import { useRoute } from 'vue-router'
+  import { storeToRefs } from 'pinia'
+  import { useAuthorStore } from '../stores/author'
+  import { usePostStore } from '../stores/post'
+  import Author from '../components/Author.vue'
+  import { User } from '../stores/author'
+
+  const route = useRoute() 
+  const { authors } = storeToRefs(useAuthorStore())
+  const { getPostsPerAuthor } = storeToRefs(usePostStore())
+  const { fetchPosts } = usePostStore()
+
+  const getAuthorByUserName = computed(() => {
+    return authors.value.find((author) => author.username === route.params.username) as User
+  })
+
+  fetchPosts()
+</script>
+
+<template>
+  <div>
+    <author 
+    :author="getAuthorByUserName" 
+    :posts="getPostsPerAuthor(getAuthorByUserName.id)">
+    </author>
+  </div> 
+</template>
